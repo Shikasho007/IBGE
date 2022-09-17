8043317454e6e81d8ce1c10ff9ee2726e44798cb
Shikasho007 <shikashogamer007@gmail.com>
2022-09-16 21:42:52 -0300

    E Acabou !

--- js/script.js
+++ js/script.js
@@ -1,43 +1,50 @@
 $(document).ready(function(e) {
-    // Ajax para pegar todos os Estados do Brasil
     $.ajax({
         type: "get",
         url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-        data: { orderBy: "nome" }, // Para deixar em ordem alfabética os nomes dos estados
+        data: { orderBy: "nome" },
         dataType: "json",
         success: function(response) {
-            // Para cada objeto contendo os estados....
             $.each(response, function(indexInArray, valueOfElement) {
-                var option = "<option>" + valueOfElement.sigla + "</option>" // Criar uma variável contendo a tag HTML
-                $("#uf").append(option) // Coloca a variável de HTML dentro da id UF
+                var option = "<option>" + valueOfElement.sigla + "</option>"
+                $("#uf").append(option)
             });
         }
     });
 
-    // Quando mudar a opção UF...
+    $('#city').hide();
+    $('#loki').hide();
+
+    $("#uf").change(function(e) {
+        e.preventDefault();
+        $("#loki").empty();
+        $('#city').show();
+        $('#loki').show();
+        let uf = $("#uf").val();
+    })
+
+
     $('#uf').change(function(e) {
         e.preventDefault();
-        $("#local").empty();
+        $("#loki").empty();
         var uf = $("#uf").val();
 
-        // Quando voltar a selecionar "Estados", volta a colocar "cidades" no select de cidades
         if (uf == 'Estados') {
             var option = "<option>Cidades</option>"
-            $("#local").append(option)
+            $("#city").append(option)
             return
         }
 
-        // Ajax para pegar todas as cidades de qualquer estado
+
         $.ajax({
             type: "get",
             url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + uf + "/municipios",
-            data: { orderBy: "nome" }, // Para deixar em ordem alfabética os nomes das cidades
+            data: { orderBy: "nome" },
             dataType: "json",
             success: function(response) {
-                // Para cada objeto contendo os cidades....
                 $.each(response, function(indexInArray, valueOfElement) {
-                    var option = "<option>" + valueOfElement.nome + "</option>" // Criar uma variável contendo a tag HTML
-                    $("#local").append(option) // Coloca a variável de HTML dentro da id local
+                    var option = "<option>" + valueOfElement.nome + "</option>"
+                    $("#loki").append(option)
                 });
             }
         });
